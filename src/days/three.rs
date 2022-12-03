8ab47fa32c4df9126d925ee5ef9f55324bede75a
Steve Edmans <30315073+steve-edmans@users.noreply.github.com>
2022-12-03 21:55:24 +0000

    Day Three Initial

---
+++ src/days/three.rs
@@ -0,0 +1,9 @@
+use advent2022::DailyChallenge;
+
+pub struct Three {}
+
+impl DailyChallenge for Three {
+    fn run(&self) {
+        println!("Day Three");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod days;
 use advent2022::DailyChallenge;
 use days::one::One;
 use days::two::Two;
+use days::three::Three;
 
 fn main() {
     println!("Advent of code 2022");
     let days: Vec<Box<dyn DailyChallenge>> = vec![
         Box::new(One {}),
-        Box::new(Two {})
+        Box::new(Two {}),
+        Box::new(Three {}),
     ];
     for day in days {
         day.run();
