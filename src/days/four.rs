7108fe3be1ec12fea6b7a5e18a9a78482a0ca9a5
Steve Edmans <30315073+steve-edmans@users.noreply.github.com>
2022-12-05 15:44:44 +0000

    Day Four Initial

---
+++ src/days/four.rs
@@ -0,0 +1,10 @@
+use std::fs;
+use advent2022::DailyChallenge;
+
+pub struct Four {}
+
+impl DailyChallenge for Four {
+    fn run(&self) {
+        println!("Day Four");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use advent2022::DailyChallenge;
 use days::one::One;
 use days::two::Two;
 use days::three::Three;
+use days::four::Four;
 
 fn main() {
     println!("Advent of code 2022");
@@ -11,6 +12,7 @@ fn main() {
         Box::new(One {}),
         Box::new(Two {}),
         Box::new(Three {}),
+        Box::new(Four {}),
     ];
     for day in days {
         day.run();
