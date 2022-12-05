d1c7f8e36bc8f6996f7003f7c656cd93fb76d4a2
Steve Edmans <30315073+steve-edmans@users.noreply.github.com>
2022-12-05 15:42:09 +0000

    Day Three Part Two

--- src/days/three.rs
+++ src/days/three.rs
@@ -6,29 +6,63 @@ pub struct Three {}
 impl DailyChallenge for Three {
     fn run(&self) {
         println!("Day Three");
-        let data: Vec<u16> = fs::read_to_string("contents/day_three.txt")
+        let rucksacks: Vec<Rucksack> = fs::read_to_string("contents/day_three.txt")
             .expect("Should have been able to read file")
             .lines()
-            // .take(10)
-            .map(|rucksack_code| Rucksack::from(rucksack_code))
-            .flat_map(|rucksack| rucksack.unwrap().priority())
+            // .take(12)
+            .map(|rucksack_code| Rucksack::from(rucksack_code).unwrap())
             .collect();
 
-        let part_one: u16 = data.iter().sum();
+        let part_one_data: Vec<u16> = rucksacks.iter()
+            .flat_map(|rucksack| rucksack.priority())
+            .collect();
+
+        let part_one: u16 = part_one_data.iter().sum();
         println!("The result of part one is {:?}", part_one);
+
+        let mut rucksacks = rucksacks.iter();
+        let mut part_two = 0;
+        while let (Some(first), Some(second), Some(third)) = (rucksacks.next(), rucksacks.next(), rucksacks.next()) {
+            let badge_type =  Rucksack::badge_item_type(first, second, third).unwrap();
+            let priority = Rucksack::priority_from_char(badge_type).unwrap();
+            part_two += priority;
+        }
+        println!("The result of part_two is {:?}", part_two);
     }
 }
 
 #[derive(Debug)]
-struct Rucksack<'a> {
-    first_compartment: &'a str,
-    second_compartment: &'a str,
+struct Rucksack {
+    first_compartment: String,
+    second_compartment: String,
 }
 
-impl Rucksack<'_> {
+impl Rucksack {
     fn from(code: &str) -> Result<Rucksack, &'static str> {
         let (first_component, second_compartment) = code.split_at(code.len() / 2);
-        Ok(Rucksack { first_compartment: first_component, second_compartment: second_compartment })
+        Ok(Rucksack {
+            first_compartment: String::from(first_component),
+            second_compartment: String::from(second_compartment)
+        })
+    }
+
+    fn badge_item_type(first: &Rucksack, second: &Rucksack, third: &Rucksack) -> Option<char> {
+        if let Some(badge_item_type) = Rucksack::check_compartment(&first.first_compartment, second, third) {
+            return Some(badge_item_type)
+        } else {
+            return Rucksack::check_compartment(&first.second_compartment, second, third);
+        }
+    }
+
+    fn check_compartment(compartment: &String, second: &Rucksack, third: &Rucksack) -> Option<char> {
+        for item in compartment.chars() {
+            if second.first_compartment.contains(item) || second.second_compartment.contains(item) {
+                if third.first_compartment.contains(item) || third.second_compartment.contains(item) {
+                    return Some(item);
+                }
+            }
+        }
+        None
     }
 
     fn first_shared_item(&self) -> Option<char> {
@@ -145,5 +179,23 @@ mod tests {
     fn sixth_rucksack() {
         let rucksack = Rucksack::from("CrZsJsPPZsGzwwsLwLmpwMDw").unwrap();
         assert_eq!(rucksack.priority(), Some(19));
+    }
+
+    #[test]
+    fn check_first_group_of_rucksacks() {
+        let first = Rucksack::from("vJrwpWtwJgWrhcsFMMfFFhFp").unwrap();
+        let second = Rucksack::from("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL").unwrap();
+        let third = Rucksack::from("PmmdzqPrVvPwwTWBwg").unwrap();
+
+        assert_eq!(Rucksack::badge_item_type(&first, &second, &third), Some('r'));
+    }
+
+    #[test]
+    fn check_second_group_of_rucksacks() {
+        let first = Rucksack::from("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn").unwrap();
+        let second = Rucksack::from("ttgJtRGJQctTZtZT").unwrap();
+        let third = Rucksack::from("CrZsJsPPZsGzwwsLwLmpwMDw").unwrap();
+
+        assert_eq!(Rucksack::badge_item_type(&first, &second, &third), Some('Z'));
     }
 }
